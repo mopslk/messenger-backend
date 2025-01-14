539ff50bb2d2ca2c19f020e506fd268c45b8a107
mopsik <mopsik.3281@gmail.com>
2025-01-15 00:13:43 +0300

    fix: Добавлены cascade on delete на связанные с чатом данные

---
+++ prisma/migrations/20250114210542_add_cascades_delete_to_schema/migration.sql
@@ -0,0 +1,29 @@
+-- DropForeignKey
+ALTER TABLE "chat_members" DROP CONSTRAINT "chat_members_chat_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "chat_members" DROP CONSTRAINT "chat_members_user_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "message_attachments" DROP CONSTRAINT "message_attachments_message_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "messages" DROP CONSTRAINT "messages_chat_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "messages" DROP CONSTRAINT "messages_user_id_fkey";
+
+-- AddForeignKey
+ALTER TABLE "messages" ADD CONSTRAINT "messages_chat_id_fkey" FOREIGN KEY ("chat_id") REFERENCES "chats"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "messages" ADD CONSTRAINT "messages_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "chat_members" ADD CONSTRAINT "chat_members_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "chat_members" ADD CONSTRAINT "chat_members_chat_id_fkey" FOREIGN KEY ("chat_id") REFERENCES "chats"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "message_attachments" ADD CONSTRAINT "message_attachments_message_id_fkey" FOREIGN KEY ("message_id") REFERENCES "messages"("id") ON DELETE CASCADE ON UPDATE CASCADE;
